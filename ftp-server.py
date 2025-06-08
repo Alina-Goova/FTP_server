1fb7eb42ab0adbfbc7bfdfa2e54f64cd5c1cc90f
Alina-Goova <147145257+Alina-Goova@users.noreply.github.com>
2025-06-08 16:22:05 +0300

    Add file transfer operations

--- ftp-server.py
+++ ftp-server.py
@@ -11,55 +11,26 @@ rm <filename> - удаляет файл
 dirname = os.path.join(os.getcwd(), 'docs')
 
 def process(req):
-    if req == 'pwd':
-        return dirname
-    elif req == 'ls':
-        return '; '.join(os.listdir(dirname))
-    elif req.startswith('cat '):
+    # ... previous commands ...
+    elif req == 'upload':
+        conn.send('READY'.encode())
+        fileinfo = conn.recv(1024).decode().split(':', 1)
+        filename, filesize = fileinfo[0], int(fileinfo[1])
+        with open(os.path.join(dirname, filename), 'wb') as f:
+            received = 0
+            while received < filesize:
+                data = conn.recv(1024)
+                f.write(data)
+                received += len(data)
+        return f'File {filename} uploaded'
+    elif req.startswith('download '):
         filename = req.split()[1]
         try:
-            with open(os.path.join(dirname, filename), 'r') as f:
-                return f.read()
+            filesize = os.path.getsize(os.path.join(dirname, filename))
+            conn.send(f'{filename}:{filesize}'.encode())
+            with open(os.path.join(dirname, filename), 'rb') as f:
+                conn.sendfile(f)
+            return 'File sent'
         except:
             return 'File not found'
-    elif req.startswith('create '):
-        parts = req.split(' ', 2)
-        if len(parts) < 3:
-            return 'Usage: create <filename> <content>'
-        filename = parts[1]
-        with open(os.path.join(dirname, filename), 'w') as f:
-            f.write(parts[2])
-        return f'File {filename} created'
-    elif req.startswith('rm '):
-        filename = req.split()[1]
-        try:
-            os.remove(os.path.join(dirname, filename))
-            return f'File {filename} removed'
-        except:
-            return 'File not found'
-    elif req.startswith('mkdir '):
-        dirpath = req.split()[1]
-        try:
-            os.mkdir(os.path.join(dirname, dirpath))
-            return f'Directory {dirpath} created'
-        except:
-            return 'Directory creation failed'
-    elif req.startswith('rmdir '):
-        dirpath = req.split()[1]
-        try:
-            os.rmdir(os.path.join(dirname, dirpath))
-            return f'Directory {dirpath} removed'
-        except:
-            return 'Directory removal failed'
-    elif req.startswith('rename '):
-        parts = req.split()
-        if len(parts) < 3:
-            return 'Usage: rename <oldname> <newname>'
-        oldname, newname = parts[1], parts[2]
-        try:
-            os.rename(os.path.join(dirname, oldname), 
-                      os.path.join(dirname, newname))
-            return f'Renamed {oldname} to {newname}'
-        except:
-            return 'Rename failed'
     return 'bad request'
