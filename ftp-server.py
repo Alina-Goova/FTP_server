8714fd6c8575c70966256ca5987b47a30f647295
Alina-Goova <147145257+Alina-Goova@users.noreply.github.com>
2025-06-08 16:20:53 +0300

    Add directory operations

--- ftp-server.py
+++ ftp-server.py
@@ -37,4 +37,29 @@ def process(req):
             return f'File {filename} removed'
         except:
             return 'File not found'
+    elif req.startswith('mkdir '):
+        dirpath = req.split()[1]
+        try:
+            os.mkdir(os.path.join(dirname, dirpath))
+            return f'Directory {dirpath} created'
+        except:
+            return 'Directory creation failed'
+    elif req.startswith('rmdir '):
+        dirpath = req.split()[1]
+        try:
+            os.rmdir(os.path.join(dirname, dirpath))
+            return f'Directory {dirpath} removed'
+        except:
+            return 'Directory removal failed'
+    elif req.startswith('rename '):
+        parts = req.split()
+        if len(parts) < 3:
+            return 'Usage: rename <oldname> <newname>'
+        oldname, newname = parts[1], parts[2]
+        try:
+            os.rename(os.path.join(dirname, oldname), 
+                      os.path.join(dirname, newname))
+            return f'Renamed {oldname} to {newname}'
+        except:
+            return 'Rename failed'
     return 'bad request'
