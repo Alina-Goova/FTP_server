b15743edf4cebe8713e267db742f439e60146ea4
Alina-Goova <147145257+Alina-Goova@users.noreply.github.com>
2025-06-08 16:19:58 +0300

    Modified ftp-server.py

--- ftp-server.py
+++ ftp-server.py
@@ -4,6 +4,8 @@ import os
 pwd - показывает название рабочей директории
 ls - показывает содержимое текущей директории
 cat <filename> - отправляет содержимое файла
+create <filename> <content> - создает файл с содержимым
+rm <filename> - удаляет файл
 '''
 
 dirname = os.path.join(os.getcwd(), 'docs')
@@ -13,23 +15,26 @@ def process(req):
         return dirname
     elif req == 'ls':
         return '; '.join(os.listdir(dirname))
+    elif req.startswith('cat '):
+        filename = req.split()[1]
+        try:
+            with open(os.path.join(dirname, filename), 'r') as f:
+                return f.read()
+        except:
+            return 'File not found'
+    elif req.startswith('create '):
+        parts = req.split(' ', 2)
+        if len(parts) < 3:
+            return 'Usage: create <filename> <content>'
+        filename = parts[1]
+        with open(os.path.join(dirname, filename), 'w') as f:
+            f.write(parts[2])
+        return f'File {filename} created'
+    elif req.startswith('rm '):
+        filename = req.split()[1]
+        try:
+            os.remove(os.path.join(dirname, filename))
+            return f'File {filename} removed'
+        except:
+            return 'File not found'
     return 'bad request'
-
-
-PORT = 6666
-
-sock = socket.socket()
-sock.bind(('', PORT))
-sock.listen()
-print("Прослушиваем порт", PORT)
-
-while True:
-    conn, addr = sock.accept()
-    
-    request = conn.recv(1024).decode()
-    print(request)
-    
-    response = process(request)
-    conn.send(response.encode())
-
-conn.close()
